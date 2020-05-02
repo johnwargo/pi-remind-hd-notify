04937dc71fdfe12ce10d9d337bd77e3662065d76
John M. Wargo <johnwargo@users.noreply.github.com>
2020-05-02 08:56:23 -0400

    Update google_calendar.py
    Finished coding new calendar processing. Not tested yet

--- google_calendar.py
+++ google_calendar.py
@@ -4,11 +4,12 @@
 # Exposes properties and methods for the Google Calendar
 ###########################################################
 # TODO: Clean up imports
+# TODO: Support working hours (off vs. free)
 # TODO: Ignore tentative appointments
 # TODO: Move reboot counter to remind.py
 
 # This project's imports (local modules)
-import status
+from status import Status
 import unicorn_hat as unicorn
 
 # other modules
@@ -108,19 +109,21 @@ class GoogleCalendar:
         # need this at its default (infinity, i think) during the registration process.
         socket.setdefaulttimeout(5)  # seconds
 
-
     @staticmethod
     def _is_busy(event):
-        # TODO: would this work: `return not event['transparency']`?
+        # TODO: would this work?
+        logging.debug('is_busy()')
+        return not event['transparency']
         # busy if transparency is missing
-        transparent = event['transparency']
-        if transparent:
-            return False
-        else:
-            return True
+        # transparent = event['transparency']
+        # if transparent:
+        #     return False
+        # else:
+        #     return True
 
     @staticmethod
     def _has_reminder(event):
+        logging.debug('has_reminder()')
         # Return true if there's a reminder set for the event
         # First, check to see if there is a default reminder set
         # Yes, I know I could have done this and the next check without using variables
@@ -140,23 +143,40 @@ class GoogleCalendar:
         return False
 
     @staticmethod
-    def _is_current_event(event):
-        # Is the event happening now?
-
-        pass
+    def _is_working_hours(now):
+        # TODO: Implement this
+        logging.debug('is_working_hours()')
+        # is the current time within working hours?
+        return True
 
     @staticmethod
-    def _is_future_event(event, time_window):
-        # is this event starting within the time window?
+    def _process_upcoming_event(event, start, time_delta):
+        logging.debug('_process_upcoming_event(event, {}, {})'.format(start, time_delta))
+        event_summary = event['summary'] if 'summary' in event else 'No Title'
+        logging.info('Found event: {}'.format(event_summary))
+        logging.info('Event starts: {}'.format(start))
+        new_event = {
+            'summary': event_summary,
+            'minutes_to_start': time_delta.total_seconds() // 60}
+        return new_event
 
-        pass
+    @staticmethod
+    def _process_upcoming_events(event_list, time_window):
+        logging.debug('_process_upcoming_events(event_list, {})'.format(time_window))
+        summary_list = []
+        nearest_time = time_window
+        for event in event_list:
+            # summary = event['summary'] if 'summary' in event else 'No Title'
+            # summary_list.append(summary)
+            summary_list.append(event['summary'] if 'summary' in event else 'No Title')
+            # find the nearest (soonest) meeting time
+            nearest_time = min(nearest_time, event['minutes_to_start'])
+        return nearest_time, ','.join(summary_list)
 
     @staticmethod
     def get_status(self, time_window):
+        logging.debug('get_status({})'.format(time_window))
         # get the status of the user's calendar
-        pass
-
-    def get_next_event(self, time_window):
         global reboot_counter
         # get all of the events on the calendar from now through 10 minutes from now
         logging.info('Getting next event')
@@ -169,81 +189,171 @@ class GoogleCalendar:
         if not self._has_error:
             # turn on a sequential CHECKING_COLOR LED to show that you're requesting data from the Google Calendar API
             unicorn.set_activity_light(unicorn.CHECKING_COLOR, True)
-            # try:
             # ask Google for the calendar entries
-            events_result = self._service.events().list(
-                # get all of them between now and 10 minutes from now
-                calendarId='primary',
-                timeMin=now.isoformat() + 'Z',
-                timeMax=then.isoformat() + 'Z',
-                singleEvents=True,
-                orderBy='startTime').execute()
-            # turn on the SUCCESS_COLOR LED so you'll know data was returned from the Google calendar API
-            unicorn.set_activity_light(unicorn.SUCCESS_COLOR, False)
-            # Get the event list
-            event_list = events_result.get('items', [])
-            # initialize this here, setting it to true later if we encounter an error
-            self._has_error = False
-            # reset the reboot counter, since everything worked so far
-            reboot_counter = 0
-            # did we get a return value?
-            if not event_list:
-                # no? Then no upcoming events at all, so nothing to do right now
-                logging.info('No entries returned')
-                return None
-            else:
-                # what time is it now?
-                current_time = pytz.utc.localize(datetime.datetime.utcnow())
-                # loop through the events in the list
-                for event in event_list:
-                    # write the event to the console
-                    logging.debug('Event: {}'.format(event))
-                    # we only care about events that have a start time
-                    start = event['start'].get('dateTime')
-                    # return the first event that has a start time
-                    # so, first, do we have a start time for this event?
-                    if start:
-                        # When does the appointment start?
-                        # Convert the string it into a Python dateTime object so we can do math on it
-                        event_start = parser.parse(start)
-                        # does the event start in the future?
-                        if current_time < event_start:
+        events_result = self._service.events().list(
+            # get all of them between now and 10 minutes from now
+            calendarId='primary',
+            timeMin=now.isoformat() + 'Z',
+            timeMax=then.isoformat() + 'Z',
+            singleEvents=True,
+            orderBy='startTime').execute()
+        # turn on the SUCCESS_COLOR LED so you'll know data was returned from the Google calendar API
+        unicorn.set_activity_light(unicorn.SUCCESS_COLOR, False)
+        # Get the event list
+        event_list = events_result.get('items', [])
+        # initialize this here, setting it to true later if we encounter an error
+        self._has_error = False
+        # reset the reboot counter, since everything worked so far
+        reboot_counter = 0
+
+        # set our base calendar status, assume we're turning the status LED off
+        current_status = Status.OFF
+        if self._is_working_hours(now):
+            current_status = Status.FREE
+
+        # Did we get any events back?
+        if not event_list:
+            # no? so nothing to do right now
+            logging.info('No calendar entries returned')
+            # Return values: num_minutes, summary_string, calendar_status
+            return 0, '', current_status
+        else:
+            # what time is it now?
+            current_time = pytz.utc.localize(datetime.datetime.utcnow())
+            # an empty list of upcoming events, will populate in the following loop
+            upcoming_events = []
+            # loop through the events in the list
+            for event in event_list:
+                # write the event to the console
+                logging.debug('Event: {}'.format(event))
+                # we only care about events that have a start time
+                start = event['start'].get('dateTime')
+                # we only want events that have a start time (skips all day events)
+                # do we have a start time for this event?
+                if start:
+                    # When does the appointment start?
+                    # Convert the string it into a Python dateTime object so we can do math on it
+                    event_start = parser.parse(start)
+                    # does the event start in the future?
+                    if current_time < event_start:
+                        time_delta = event_start - current_time
+                        new_event = self._process_upcoming_event(event, start, time_delta)
+                        logging.debug('New Event: {}'.format(new_event))
+                        # we have an upcoming event
+                        if self._reminder_only:
                             # only use events that have a reminder set
                             if self._has_reminder(event):
-                                # no? So we can use it
-                                event_summary = event['summary'] if 'summary' in event else 'No Title'
-                                logging.info('Found event: {}'.format(event_summary))
-                                logging.info('Event starts: {}'.format(start))
-                                # figure out how soon it starts
-                                time_delta = event_start - current_time
-                                # Round to the nearest minute and return with the object
-                                event['num_minutes'] = time_delta.total_seconds() // 60
-                                return event
-        # except Exception as e:
-        #     # Something went wrong, tell the user (just in case they have a monitor on the Pi)
-        #     logging.error('Exception type: {}'.format(type(e)))
-        #     # not much else we can do here except to skip this attempt and try again later
-        #     logging.error('Error: {}'.format(sys.exc_info()[0]))
-        #     # light up the array with FAILURE_COLOR LEDs to indicate a problem
-        #     unicorn.flash_all(1, 2, unicorn.FAILURE_COLOR)
-        #     # now set the current_activity_light to FAILURE_COLOR to indicate an error state
-        #     # with the last reading
-        #     unicorn.set_activity_light(unicorn.FAILURE_COLOR, False)
-        #     # we have an error, so make note of it
-        #     self._has_error = True
-        #     # check to see if reboot is enabled
-        #     if self._use_reboot_counter:
-        #         # increment the counter
-        #         reboot_counter += 1
-        #         logging.info('Incrementing the reboot counter ({})'.format(reboot_counter))
-        #         # did we reach the reboot threshold?
-        #         if reboot_counter == self._reboot_counter_limit:
-        #             # Reboot the Pi
-        #             for i in range(1, 10):
-        #                 logging.info('Rebooting in {} seconds'.format(i))
-        #                 time.sleep(1)
-        #             os.system("sudo reboot")
+                                upcoming_events.append(new_event)
+                        else:
+                            # add the event to our upcoming event list
+                            upcoming_events.append(new_event)
+                    else:
+                        # we have an ongoing/current event
+                        # Are we processing busy events only?
+                        if self._busy_only:
+                            # then is the user marked busy for this event?
+                            if self.is_busy(event):
+                                # add the event to our current event list
+                                current_status = Status.BUSY
+                        else:
+                            if self.is_busy(event):
+                                # add the event to our current event list
+                                current_status = min(current_status, Status.BUSY)
+                            else:
+                                current_status = min(current_status, Status.TENTATIVE)
+            # start processing our lists
+            # Return values: num_minutes, summary_string, calendar_status
+            num_minutes, summary_string = self._process_upcoming_events(upcoming_events, time_window)
+            return num_minutes, summary_string, current_status
 
-        # if we got this far and haven't returned anything, then there's no appointments in the specified time
-        # range, or we had an error, so...
-        return None
+    # def get_next_event(self, time_window):
+    #     global reboot_counter
+    #     # get all of the events on the calendar from now through 10 minutes from now
+    #     logging.info('Getting next event')
+    #     # this 'now' is in a different format (UTC)
+    #     now = datetime.datetime.utcnow()
+    #     # Calculate a time search_limit from now
+    #     then = now + datetime.timedelta(minutes=time_window)
+    #     # if we don't have an error from the previous attempt, then change the LED color
+    #     # otherwise leave it alone (it should already be red, so it will stay that way).
+    #     if not self._has_error:
+    #         # turn on a sequential CHECKING_COLOR LED to show that you're requesting data from the Google Calendar API
+    #         unicorn.set_activity_light(unicorn.CHECKING_COLOR, True)
+    #     try:
+    #         # ask Google for the calendar entries
+    #         events_result = self._service.events().list(
+    #             # get all of them between now and 10 minutes from now
+    #             calendarId='primary',
+    #             timeMin=now.isoformat() + 'Z',
+    #             timeMax=then.isoformat() + 'Z',
+    #             singleEvents=True,
+    #             orderBy='startTime').execute()
+    #         # turn on the SUCCESS_COLOR LED so you'll know data was returned from the Google calendar API
+    #         unicorn.set_activity_light(unicorn.SUCCESS_COLOR, False)
+    #         # Get the event list
+    #         event_list = events_result.get('items', [])
+    #         # initialize this here, setting it to true later if we encounter an error
+    #         self._has_error = False
+    #         # reset the reboot counter, since everything worked so far
+    #         reboot_counter = 0
+    #         # did we get a return value?
+    #         if not event_list:
+    #             # no? Then no upcoming events at all, so nothing to do right now
+    #             logging.info('No entries returned')
+    #             return None
+    #         else:
+    #             # what time is it now?
+    #             current_time = pytz.utc.localize(datetime.datetime.utcnow())
+    #             # loop through the events in the list
+    #             for event in event_list:
+    #                 # write the event to the console
+    #                 logging.debug('Event: {}'.format(event))
+    #                 # we only care about events that have a start time
+    #                 start = event['start'].get('dateTime')
+    #                 # return the first event that has a start time
+    #                 # so, first, do we have a start time for this event?
+    #                 if start:
+    #                     # When does the appointment start?
+    #                     # Convert the string it into a Python dateTime object so we can do math on it
+    #                     event_start = parser.parse(start)
+    #                     # does the event start in the future?
+    #                     if current_time < event_start:
+    #                         # only use events that have a reminder set
+    #                         if self._has_reminder(event):
+    #                             # no? So we can use it
+    #                             event_summary = event['summary'] if 'summary' in event else 'No Title'
+    #                             logging.info('Found event: {}'.format(event_summary))
+    #                             logging.info('Event starts: {}'.format(start))
+    #                             # figure out how soon it starts
+    #                             time_delta = event_start - current_time
+    #                             # Round to the nearest minute and return with the object
+    #                             event['num_minutes'] = time_delta.total_seconds() // 60
+    #                             return event
+    #     except Exception as e:
+    #         # Something went wrong, tell the user (just in case they have a monitor on the Pi)
+    #         logging.error('Exception type: {}'.format(type(e)))
+    #         # not much else we can do here except to skip this attempt and try again later
+    #         logging.error('Error: {}'.format(sys.exc_info()[0]))
+    #         # light up the array with FAILURE_COLOR LEDs to indicate a problem
+    #         unicorn.flash_all(1, 2, unicorn.FAILURE_COLOR)
+    #         # now set the current_activity_light to FAILURE_COLOR to indicate an error state
+    #         # with the last reading
+    #         unicorn.set_activity_light(unicorn.FAILURE_COLOR, False)
+    #         # we have an error, so make note of it
+    #         self._has_error = True
+    #         # check to see if reboot is enabled
+    #         if self._use_reboot_counter:
+    #             # increment the counter
+    #             reboot_counter += 1
+    #             logging.info('Incrementing the reboot counter ({})'.format(reboot_counter))
+    #             # did we reach the reboot threshold?
+    #             if reboot_counter == self._reboot_counter_limit:
+    #                 # Reboot the Pi
+    #                 for i in range(1, 10):
+    #                     logging.info('Rebooting in {} seconds'.format(i))
+    #                     time.sleep(1)
+    #                 os.system("sudo reboot")
+    #
+    #     # if we got this far and haven't returned anything, then there's no appointments in the specified time
+    #     # range, or we had an error, so...
+    #     return None
